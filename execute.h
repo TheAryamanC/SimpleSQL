edf4ad7ae67acd88bf2ecc6fe0e7bfa618fcf2d7
TheAryamanC <aryamanchawla279@gmail.com>
2023-10-16 10:05:18 -0500

    initial files

---
+++ execute.h
@@ -0,0 +1,34 @@
+/*execute.h*/
+
+//
+// Project: Execution of queries for SimpleSQL
+//
+// Aryaman Chawla
+// Northwestern University
+// CS 211, Winter 2023
+//
+
+#pragma once
+
+#include "analyzer.h"
+#include "ast.h"
+#include "database.h"
+#include "execute.h"
+#include "parser.h"
+#include "resultset.h"
+#include "scanner.h"
+#include "token.h"
+#include "tokenqueue.h"
+#include "util.h"
+
+//
+// #include header files needed to compile function declarations
+//
+
+//
+// function declarations:
+//
+
+// Executing the query
+void execute_query(struct Database *db, struct QUERY *query,
+                   struct ResultSet *rSet);
